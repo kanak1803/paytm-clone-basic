7cea4abb4acdead03cee943f34420d73dd854407
Kanak Ramapure <kanak1803@gmail.com>
2025-05-16 14:09:07 +0530

    add zod validator

---
+++ backend/validators/userValidator.js
@@ -0,0 +1,10 @@
+const { z } = require("zod");
+
+const registerUserSchema = z.object({
+  firstName: z.string().min(2, "First Name must be at least 3 character long"),
+  lastName: z.string().min(3, "Last Name must be at least 3 character long"),
+  email: z.string().email("Invalid email"),
+  password: z.string().min(6, "Password must be at least 6 character long"),
+});
+
+module.exports = { registerUserSchema };
