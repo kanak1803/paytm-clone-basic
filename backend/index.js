72d90bc75fabaf9c257fabbcce3dff65e48f64c1
Kanak Ramapure <kanak1803@gmail.com>
2025-05-16 15:55:37 +0530

    added user routes

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
@@ -7,6 +8,8 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 app.use(express.json());
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running");
